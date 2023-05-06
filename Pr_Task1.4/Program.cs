09a6b9e7e3687088f06fb96dd39124c97b1bd939
RaGaLiK <RaGaLiK@DESKTOP-58NBP1E>
2023-05-06 17:51:28 +0400

    Добавьте файлы проекта.

---
+++ Pr_Task1.4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    static int RomanToInt(string roman)
+    {
+        Dictionary<char, int> romanValues = new Dictionary<char, int>()
+    {
+        {'I', 1},
+        {'V', 5},
+        {'X', 10},
+        {'L', 50},
+        {'C', 100},
+        {'D', 500},
+        {'M', 1000}
+    };
+
+        int result = 0;
+
+        for (int i = 0; i < roman.Length; i++)
+        {
+            if (i + 1 < roman.Length && romanValues[roman[i]] < romanValues[roman[i + 1]])
+            {
+                result -= romanValues[roman[i]];
+            }
+            else
+            {
+                result += romanValues[roman[i]];
+            }
+        }
+
+        return result;
+    }
+
+    string romanNumeral;
+    romanNumeral = Console.ReadLine();
+    int number = RomanToInt(romanNumeral);
+    Console.WriteLine($"Обычное целое число, соответствующее римской цифре {romanNumeral}, равно: {number}");
+
